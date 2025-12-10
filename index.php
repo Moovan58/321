33dea3ce854042adfedd1011a701bba824b8d9f7
Moovan58 <vanabugreev@gmail.com>
2025-12-10 17:11:44 +0300

    new commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Моя страница </title>
+		<link rel="stylesheet" href='mycss.css'>
+		<link rel="shortcut icon" href='favicon.ico'>
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Результат вычисления площадь трапеции с длинно верхнего основа 3, ниженго основания 5 и высотой 4 равна ";
+				echo 1/2 * (3 + 5) * 4;
+			?>
+		</div>
+	</body>
+</html>
